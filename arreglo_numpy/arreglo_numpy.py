7175822e7b1c40544132b8c9afd972e47c923df1
ramonezorrilla <ramoenzorrilla@gmail.com>
2025-01-23 20:15:53 -0400

    cambios

--- arreglo_numpy/arreglo_numpy.py
+++ arreglo_numpy/arreglo_numpy.py
@@ -4,3 +4,20 @@ import numpy as np
 array = np.array([1, 2, 3, 4])
 print(array * 2)  # [2, 4, 6, 8]
 
+
+
+a = np.array([1, 2, 3, 4])
+b = np.array([10, 20, 30, 40])
+print(a + b)  # [11, 22, 33, 44]
+
+matriz_a = np.array([[1, 2], [3, 4]])
+matriz_b = np.array([[5, 6], [7, 8]])
+print(np.dot(matriz_a, matriz_b))  # Producto matricial
+
+datos = np.array([10, 20, 30, 40, 50])
+print(np.mean(datos))  # Media: 30.0
+print(np.std(datos))   # Desviación estándar
+
+numeros_aleatorios = np.random.rand(5)  # Genera 5 números entre 0 y 1
+print(numeros_aleatorios)
+
